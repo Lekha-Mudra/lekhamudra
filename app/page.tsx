43f7ad4f81549e8216f7eddcdf7e94423991da2c
Shlok Koirala <shlokkoirala19@gmail.com>
2024-10-28 21:14:13 +0545

    fixed toggle

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import LoginPage from "./auth/page";
 import { Button } from "@/app/ui/button"
 import { Moon, Sun, FileText } from 'lucide-react'
 import { useState } from "react";
+import clsx from "clsx";
 
 
 export default function Home() {
@@ -14,15 +15,19 @@ export default function Home() {
   }
 
   return (
+    <>
     <div className={`h-screen w-screen ${isDarkMode ? 'bg-black' : ''}`}>
-        <div className="absolute top-4 right-4">
-        <Button variant="outline" size="sm" onClick={toggleTheme}>
-          {isDarkMode ? <Sun className="h-[1.2rem] w-[1.2rem] text-white" /> : <Moon className="h-[1.2rem] w-[1.2rem]" />}
+        <div className= {clsx( "absolute top-4 right-4",
+          { 'hover: text-black': isDarkMode },
+        )}>
+        <Button variant= "outline" size="sm" onClick={toggleTheme}>
+          {isDarkMode ? <Sun className="h-[1.2rem] w-[1.2rem] text-white hover:text-black" /> : <Moon className="h-[1.2rem] w-[1.2rem]" />}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </div>
       <LoginPage isDarkMode={isDarkMode} />
 
     </div>
+    </>
   );
 }
